1def92493f9403ad747c48c668419f0d3db15243
Monique Rio <mrio@umich.edu>
2023-02-02 17:34:42 -0500

    adds allow-plugins step to post install

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,4 @@
+docker-compose exec drupal composer config allow-plugins true
 docker-compose exec drupal composer require drush/drush
 
 docker-compose exec database mysqldump -u user -ppassword drupal > db/starter.sql
