0a015f81978ff3406e6eafe391ce4f4a57f985ad
Monique Rio <mrio@umich.edu>
2023-02-21 10:29:16 -0500

    Updates to drupal 10

--- init.sh
+++ init.sh
@@ -5,11 +5,18 @@ else
   cp .env-example .env
 fi
 
-echo "📋 copy composer.json and composer.lock from drupal"
-docker run --rm drupal:9-apache cat composer.json > ./composer.json
-docker run --rm drupal:9-apache cat composer.lock > ./composer.lock
+if [ -f "composer.json" ]; then
+  echo "📄 composer.json exists. Leaving alone."
+else
+  echo "📋 copy composer.json and composer.lock from drupal"
+  docker run --rm drupal:10-apache cat composer.json > ./composer.json
+  docker run --rm drupal:10-apache cat composer.lock > ./composer.lock
+fi
 
 ./chgrp_to_www.sh
 
 echo "🚢 Build docker images"
 docker-compose build
+
+echo "📦 Composer install"
+docker-compose run --rm web composer install
